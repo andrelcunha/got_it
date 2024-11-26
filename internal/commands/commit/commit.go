aa1ecc2194c90bb43fdc9b0e476cbcc0f6eb16b2
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-11-26 18:55:47 -0300

    Generating (parial) commit metadata
    Fetch user data for author and commiter
    close #11 #12

--- internal/commands/commit/commit.go
+++ internal/commands/commit/commit.go
@@ -1,34 +1,113 @@
 package commit
 
 import (
+	"fmt"
 	"got_it/internal/commands/config"
 	"got_it/internal/models"
+	"time"
 )
 
 type Commit struct {
-	conf     *config.Config
-	message  string
-	userData *models.User
+	conf       *config.Config
+	commitData *models.CommitData
 }
 
 func NewCommit(message string) *Commit {
 	conf := config.NewConfig()
-	userData := &models.User{
-		User:  conf.GetUserName(),
-		Email: conf.GetUserEmail(),
+	commitData := &models.CommitData{
+		Message: message,
 	}
-	userData.User = conf.GetUserName()
+
 	return &Commit{
-		message:  message,
-		conf:     conf,
-		userData: userData,
+		conf:       conf,
+		commitData: commitData,
 	}
 }
 
-func (co *Commit) GetMessage() string {
-	return co.message
+// Execute is a shortcut for creating a new commit and running it
+func Execute(message string) {
+	co := NewCommit(message)
+	co.RunCommit()
 }
 
-func (co *Commit) GetUserAndEmail() (string, string) {
-	return co.conf.GetUserName(), co.conf.GetUserEmail()
+func (co *Commit) RunCommit() (string, error) {
+	err := co.FetchTree()
+	if err != nil {
+		fmt.Println("Error fetching tree:", err)
+		return "", err
+	}
+
+	err = co.FetchParent()
+	if err != nil {
+		fmt.Println("Error fetching parent:", err)
+		return "", err
+	}
+
+	err = co.FetchAuthorData(co.commitData)
+	if err != nil {
+		fmt.Println("Error fetching author data:", err)
+		return "", err
+	}
+
+	err = co.FetchCommitterData(co.commitData)
+	if err != nil {
+		fmt.Println("Error fetching committer data:", err)
+		return "", err
+	}
+
+	commitMetadata := co.FormatCommitMetadata(co.commitData)
+
+	return commitMetadata, nil
+}
+
+func (co *Commit) FetchTree() error {
+	// tree := co.conf.GetTree()
+	// commitData.Tree = tree
+	return nil
+}
+
+func (co *Commit) FetchParent() error {
+	// parent := co.conf.GetParent()
+	// commitData.Parent = parent
+	return nil
+}
+
+func (co *Commit) FetchAuthorData(commitData *models.CommitData) error {
+	authorName := co.conf.GetUserName()
+	authorEmail := co.conf.GetUserEmail()
+	timestamp := time.Now()
+
+	commitData.AuthorName = authorName
+	commitData.AuthorEmail = authorEmail
+	commitData.AuthorDate = timestamp.Format(time.RFC3339)
+	return nil
+}
+
+func (co *Commit) FetchCommitterData(commitData *models.CommitData) error {
+	committerName := co.conf.GetUserName()
+	committerEmail := co.conf.GetUserEmail()
+	timestamp := time.Now()
+
+	commitData.CommitterName = committerName
+	commitData.CommitterEmail = committerEmail
+	commitData.CommitterDate = timestamp.Format(time.RFC3339)
+	return nil
+}
+
+func (co *Commit) FormatCommitMetadata(commitData *models.CommitData) string {
+	var commitStr string
+
+	// Tree
+	commitStr += fmt.Sprintf("tree %s\n", commitData.Tree)
+	// Parent
+	commitStr += fmt.Sprintf("parent %s\n", commitData.Parent)
+	// Author
+	commitStr += fmt.Sprintf("author %s <%s> %s\n", commitData.AuthorName, commitData.AuthorEmail, commitData.AuthorDate)
+	// Committer
+	commitStr += fmt.Sprintf("committer %s <%s> %s\n", commitData.CommitterName, commitData.CommitterEmail, commitData.CommitterDate)
+	// Empty line
+	commitStr += "\n"
+	// Message
+	commitStr += fmt.Sprintf("\n%s\n", commitData.Message)
+	return commitStr
 }
--- internal/commands/commit/commit_test.go
+++ internal/commands/commit/commit_test.go
@@ -5,6 +5,7 @@ import (
 	"got_it/internal/commands/config"
 	init_ "got_it/internal/commands/init"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -31,25 +32,50 @@ func TestGetUserAndEmail(t *testing.T) {
 	setKeyValue("user.name", shouldBeUser, t)
 	setKeyValue("user.email", shouldBeEmail, t)
 
-	// Call the GetUserAndEmail function
-	user, email := commit.GetUserAndEmail()
+	commitMetadata, err := commit.RunCommit()
+	if err != nil {
+		t.Errorf("Error running commit: %v", err)
+	}
+	if commitMetadata == "" {
+		t.Errorf("Commit metadata is empty")
+	}
 
-	// Check if the returned values are correct
-	if user != shouldBeUser || email != shouldBeEmail {
-		t.Errorf("GetUserAndEmail() = (%s, %s), want (%s, %s)", user, email, shouldBeUser, shouldBeEmail)
+	// Check for lines with prefixes `tree` and `parent`
+	if !strings.Contains(commitMetadata, "tree ") {
+		t.Errorf("Commit metadata does not contain a 'tree' line")
+	}
+	if !strings.Contains(commitMetadata, "parent ") {
+		t.Errorf("Commit metadata does not contain a 'parent' line")
+	}
+	// Check if author and committer are as expected without timestamps
+	expectedAuthorPrefix := fmt.Sprintf("author %s <%s>", shouldBeUser, shouldBeEmail)
+	expectedCommitterPrefix := fmt.Sprintf("committer %s <%s>", shouldBeUser, shouldBeEmail)
+	if !strings.Contains(commitMetadata, expectedAuthorPrefix) {
+		t.Errorf("Commit metadata does not contain expected author prefix")
+		t.Errorf("Commit metadata: %s", commitMetadata)
+	}
+	if !strings.Contains(commitMetadata, expectedCommitterPrefix) {
+		t.Errorf("Commit metadata does not contain expected committer prefix")
+	}
+	// Check if the commit message is as expected
+	if !strings.Contains(commitMetadata, "test commit") {
+		t.Errorf("Commit metadata does not contain the expected commit message")
 	}
 }
 
+// GetConfig is a helper function to get the config for testing
 func (co *Commit) GetConfig(t *testing.T) *config.Config {
 	t.Helper()
 	return co.conf
 }
 
+// SetConfig is a helper function to set the config for testing
 func (co *Commit) SetConfig(conf *config.Config, t *testing.T) {
 	t.Helper()
 	co.conf = conf
 }
 
+// InitializeRepo is a helper function to initialize the Got_it repository
 func initializeRepo(t *testing.T) {
 	t.Helper()
 	// Initialize the Got_it repository
@@ -57,6 +83,7 @@ func initializeRepo(t *testing.T) {
 	i.InitRepo()
 }
 
+// setKeyValue is a helper function to set a key-value pair in the Got_it configuration
 func setKeyValue(key, value string, t *testing.T) {
 	t.Helper()
 	config := config.NewConfig()
