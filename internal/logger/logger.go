4357d8ce076a0b5eb11994fbf61f77c8d6f362d7
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-11-28 21:18:05 -0300

    feat: add `debug`mode to logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -13,6 +13,11 @@ func NewLogger(verbose bool) *Logger {
 	return &Logger{verbose: verbose}
 }
 
+// Set Debug to true to enable debug logging.
+func (l *Logger) SetDebug(debug bool) {
+	l.debug = debug
+}
+
 func (l *Logger) Log(format string, args ...interface{}) {
 	if l.verbose {
 		log.Printf(format, args...)
