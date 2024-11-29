4e4557cc2ba2e5b35e34896b37bab4ba72742393
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-11-29 15:55:04 -0300

    feat: 'type safe' for positions at index entries

--- internal/models/index.go
+++ internal/models/index.go
@@ -1,6 +1,25 @@
 package models
 
 type IndexEntry struct {
-	FilePath string
-	FileHash string
+	Path string
+	Hash string
+}
+
+type indexValue int
+
+const (
+	pathValue indexValue = iota
+	hashValue
+)
+
+type IndexKey string
+
+const (
+	PathKey IndexKey = "path"
+	HashKey IndexKey = "hash"
+)
+
+var IndexKeyValue = map[IndexKey]int{
+	"path": int(pathValue),
+	"hash": int(hashValue),
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -62,9 +62,9 @@ func ReadIndex(indexFile string) (map[string]string, error) {
 		line = strings.TrimSpace(line)
 		parts := strings.Split(line, " ")
 		if len(parts) == 2 {
-			entry.FilePath = parts[0]
-			entry.FileHash = parts[1]
-			stagedFiles[entry.FilePath] = entry.FileHash
+			entry.Path = parts[models.IndexKeyValue[models.PathKey]]
+			entry.Hash = parts[models.IndexKeyValue[models.HashKey]]
+			stagedFiles[entry.Path] = entry.Hash
 		}
 	}
 	if err := scanner.Err(); err != nil {
